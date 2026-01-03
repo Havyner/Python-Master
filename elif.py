ccb24b247b884e665b66370ec134463364fb1691
Havyner <havy012_@hotmail.com>
2026-01-03 00:43:03 -0300

    elif

---
+++ elif.py
@@ -0,0 +1,10 @@
+linguagem = input('Digite uma linguagem de programação: ')
+
+if linguagem == 'Python':
+    print('Python e legal!')
+elif linguagem == 'Javascript':
+    print('Javascript e bom!')
+elif linguagem == 'Cobol':
+    print('Cobol e antigo!')
+else:
+    print('Linguagem não identificada!')
