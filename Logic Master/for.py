5df3478a4dfa406772ba4831c6d9b9e32e2b8dac
Havyner <havy012_@hotmail.com>
2026-01-03 03:19:05 -0300

    atualizando pastas

---
+++ Logic Master/for.py
@@ -0,0 +1,8 @@
+"""lista = [1, 2, 3, 4, 5]
+
+for numero in lista:
+    print(numero)
+"""
+
+for i in range (10):
+    print(i)
---
+++ Logic Master/ifelse.py
@@ -0,0 +1,6 @@
+idade = int(input('Digite a sua idade: '))
+
+if (idade >= 18):
+    print('Você e maior de idade!!')
+else:
+    print('Você e menor de idade!!')
