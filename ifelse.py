ff25a88a1656be2d436eda1ad1142bbcb149798e
Havyner <havy012_@hotmail.com>
2026-01-04 20:44:52 -0300

    certificado Logic Master

--- ifelse.py
+++
@@ -1,6 +0,0 @@
-idade = int(input('Digite a sua idade: '))
-
-if (idade >= 18):
-    print('Você e maior de idade!!')
-else:
-    print('Você e menor de idade!!')
--- modulo 01/for.py
+++
@@ -1,8 +0,0 @@
-"""lista = [1, 2, 3, 4, 5]
-
-for numero in lista:
-    print(numero)
-"""
-
-for i in range (10):
-    print(i)
